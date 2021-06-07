d2d6474293a25867bb4ffc7dd736cfa4e92c2688
Kirill Romanenko <romanenko.kirill1995@gmail.com>
2021-06-07 23:07:30 +0300

    create functions

--- progress-chaincode.js
+++ progress-chaincode.js
@@ -32,6 +32,20 @@ class AcademicPerformance extends Contract {
                 NameTeacher: 'Иван',
                 PatronymicTeacher: 'Иванович',
             },
+            {
+                ID: 'asset3',
+                Date: 'May 8, 2021 7:54 PM',
+                NumberOfGroup: 'КТбо1-1',
+                SurnameStudent: 'Романенко',
+                NameStudent: 'Кирилл',
+                PatronymicStudent: 'Сергеевич',
+                EducationalSubject: 'Системный анализ',
+                TypeOfWork: 'Практическая работа №3',
+                NumberOfPoints: 6.5,
+                SurnameTeacher: 'Иванов',
+                NameTeacher: 'Иван',
+                PatronymicTeacher: 'Иванович',
+            },
         ];
 
         for (const asset of assets) {
@@ -41,10 +55,10 @@ class AcademicPerformance extends Contract {
         }
     }
 
-    // GetAllAssets returns all assets found in the world state.
+    // returns all assets 
     async GetAllAssets(ctx) {
         const allResults = [];
-        // range query with empty string for startKey and endKey does an open-ended query of all assets in the chaincode namespace.
+    
         const iterator = await ctx.stub.getStateByRange('', '');
         let result = await iterator.next();
         while (!result.done) {
@@ -60,6 +74,59 @@ class AcademicPerformance extends Contract {
             result = await iterator.next();
         }
         return JSON.stringify(allResults);
+    }
+    // new asset 
+    async CreateAsset(ctx, id, date, numberOfGroup, surnameStudent, nameStudent, 
+        patronymicStudent, educationalSubject, typeOfWork, numberOfPoints, surnameTeacher, nameTeacher, patronymicTeacher) {
+        const asset = {
+            ID: id,
+            Date: date,
+            NumberOfGroup: numberOfGroup,
+            SurnameStudent: surnameStudent,
+            NameStudent: nameStudent,
+            PatronymicStudent: patronymicStudent,
+            EducationalSubject: educationalSubject,
+            TypeOfWork: typeOfWork,
+            NumberOfPoints: numberOfPoints,
+            SurnameTeacher: surnameTeacher,
+            NameTeacher: nameTeacher,
+            PatronymicTeacher: patronymicTeacher,
+        };
+        await ctx.stub.putState(id, Buffer.from(JSON.stringify(asset)));
+        return JSON.stringify(asset);
+    }
+    // returns the asset id.
+    async ReadAsset(ctx, id) {
+        const assetJSON = await ctx.stub.getState(id); 
+        if (!assetJSON || assetJSON.length === 0) {
+            throw new Error(`The asset ${id} does not exist`);
+        }
+        return assetJSON.toString();
+    }
+    // updates asset 
+    async UpdateAsset(ctx, id, date, numberOfGroup, surnameStudent, nameStudent, 
+        patronymicStudent, educationalSubject, typeOfWork, numberOfPoints, surnameTeacher, nameTeacher, patronymicTeacher) {
+        const exists = await this.AssetExists(ctx, id);
+        if (!exists) {
+            throw new Error(`The asset ${id} does not exist`);
+        }
+
+        // overwriting original asset with new asset
+        const updatedAsset = {
+            ID: id,
+            Date: date,
+            NumberOfGroup: numberOfGroup,
+            SurnameStudent: surnameStudent,
+            NameStudent: nameStudent,
+            PatronymicStudent: patronymicStudent,
+            EducationalSubject: educationalSubject,
+            TypeOfWork: typeOfWork,
+            NumberOfPoints: numberOfPoints,
+            SurnameTeacher: surnameTeacher,
+            NameTeacher: nameTeacher,
+            PatronymicTeacher: patronymicTeacher,
+        };
+        return ctx.stub.putState(id, Buffer.from(JSON.stringify(updatedAsset)));
     }
 
     
